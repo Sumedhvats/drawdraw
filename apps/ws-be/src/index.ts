c510f982af4deef972409e5c1fcefdc8cd892e96
Sumedh <Sumedhvats2004@gmail.com>
2025-05-17 20:27:50 +0530

    added basic business logic

---
+++ apps/ws-be/src/index.ts
@@ -0,0 +1,19 @@
+import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (ws) => {
+  const url = ws.url;
+  if (!url) {
+    return;
+  }
+  const queryParam = new URLSearchParams(url.split("?")[1]);
+  const token = queryParam.get("token") as string;
+  const decoded = jwt.verify(token, "alsjdfl");
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+  ws.on("message", (data) => {
+    ws.send("pong ");
+  });
+});
